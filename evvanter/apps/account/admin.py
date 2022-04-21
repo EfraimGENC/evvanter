5dbba20a2e402544ab913824ff7f14ae0fa62ed4
EfraimGENC <efraim@kavimdigital.com>
2022-04-22 00:50:51 +0300

    AbstractBaseUser

--- evvanter/apps/account/admin.py
+++ evvanter/apps/account/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+User = get_user_model()
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+    list_display = ['email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'date_joined']
+    list_filter = ['groups', 'is_active', 'is_staff', 'is_superuser']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-is_superuser', '-is_staff', 'date_joined', '-id']
+    filter_horizontal = ()
